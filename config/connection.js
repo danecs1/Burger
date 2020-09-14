142ac811c21bea2bb5708ba9c186e4f57ba8d4ff
Chris Scott <scott.chriss@gmail.com>
2020-09-14 07:47:13 -0400

    modified connectionJS

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,15 @@ const mysql = require('mysql');
 let connection;
 
 console.log('PROCESS', process.env.NODE_ENV);
-if (process.env.NODE_ENV !== 'development') {
-    connection = mysql.createConnection(process.env.DATABASE_URL)
-}
 
-connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'burgers_db'
-});
+connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
+// connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'burgers_db'
+// });
 
 connection.connect(function (err) {
     if (err) {
